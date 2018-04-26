f1264329104cb9ad58910f602465ea53c85e91dd
Churn Ken Lee <ckl055@ucsd.edu>
2018-04-25 20:11:45 -0700

    Added more code

--- Wieland_HW3/Ken/code/Ken_Q5.jl
+++ Wieland_HW3/Ken/code/Ken_Q5.jl
@@ -3,10 +3,36 @@
 # Change working directory
 cd("C:/Users/churn/Documents/UCSD/Spring_2018/ECON_210C/Macro-Homework/Wieland_HW3/Ken")
 
-const α = 0.33
-const β = 0.99
-const δ = 0.02
-const ρ = 0.9
-YKss = (1/β - 1 + δ)/α
-KYss = 1/YKss
-IYss = δ*KYss
+# Define a function that computes the residuals of the 4 equations.
+function Residuals(vck, vcg, vkk, vkg)
+    α = 0.33
+    β = 0.99
+    δ = 0.02
+    ρ = 0.9
+    η = 0.5
+    GYss = 0.2
+    YKss = (1/β - 1 + δ)/α
+    KYss = 1/YKss
+    IYss = δ*KYss
+    CYss = 1 - IYss - GYss
+
+    eulerLHSK = vck*vkk - vck
+    eulerLHSG = vck*vkg + vcg*ρ - vcg
+    eulerRHSconstant = α*YKss/(α*YKss + 1 - δ)
+    eulerRHSK = CYss*vck*vkk + IYss*(vkk-1+δ)*vkk/δ - vkk
+    eulerRHSG = CYss*vck*vkg + CYss*vcg*ρ + IYss*(vkk-1+δ)*vkg/δ + IYss*vkg*ρ/δ + GYss*ρ - vkg
+
+    llLHSK = vck - (1/η + 1)*α/(1-α)
+    llLHSG = vcg
+    llRHSconstant = (-α-1/η)/(1-α)
+    llRHSK = CYss*vck + IYss*(vkk-1+δ)/δ
+    llRHSG = CYss*vcg + IYss*vkg/δ + GYss
+
+    diff = Array{Float64}(4)
+    diff[1] = eulerLHSK - eulerRHSconstant*eulerRHSK
+    diff[2] = eulerLHSG - eulerRHSconstant*eulerRHSG
+    diff[3] = llLHSK - llRHSconstant*llRHSK
+    diff[4] = llLHSG - llRHSconstant*llRHSG
+
+    return diff
+end
